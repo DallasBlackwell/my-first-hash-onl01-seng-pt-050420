cab3cc6f487e027c7d3d010ff1c2085ab4044578
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-15 23:26:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,14 +20,14 @@ def retrieval
 shipping_manifest["oil_paintings"]
 end
 
-# def adding
-#   shipping_manifest = {
-#     "whale bone corsets" => 5, 
-#     "porcelain vases" => 2, 
-#     "oil paintings" => 3
-#     }
-#   shipping_manifest["muskets" => 2]
-#   shipping_manifest["gun powder" => 4]
-#   shipping_manifest
+def adding
+  shipping_manifest = {
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+    }
+  shipping_manifest["muskets" => 2]
+  shipping_manifest["gun powder" => 4]
+  shipping_manifest
   
-# end
+end
