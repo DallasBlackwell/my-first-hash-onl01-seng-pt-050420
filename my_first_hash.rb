5bfd6d8bbf0d74ba9f7ce1a51965f78a9dfc5597
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-15 23:22:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,29 +13,22 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+    }
 shipping_manifest["oil_paintings"]
-
 end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+    }
+  shipping_manifest["muskets" => 2]
+  shipping_manifest["gun powder" => 4]
+  shipping_manifest
   # return the shipping_manifest hash below
 
 end
